fc239b3076ed30503b7e55aa93315154df5b6481
oleg <tsukanov.regula@gmail.com>
2023-01-09 22:00:33 +0500

    made digit sum calc

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,21 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        //4587
+        int d1 = number / 1000; // 4 ++
+        int n1 = d1 * 1000; // 4000
+        int n2 = number - n1; // 587
+        int d2 = n2 / 100; // 5 ++
+        int n3 = d2 * 100; // 500
+        int n4 = n2 - n3; // 87
+        int d3 = n4 / 10; // 8 ++
+        int n5 = d3 * 10; // 80
+        int d4 = n4 - n5; // 7 ++
+
+        int result = d1 + d2 + d3 + d4;
+
+        System.out.println(result);
+
     }
 }
