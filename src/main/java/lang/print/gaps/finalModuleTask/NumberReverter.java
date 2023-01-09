523d06f7e0fe8d0d24be463f9c20477f739052be
oleg <tsukanov.regula@gmail.com>
2023-01-09 21:50:52 +0500

    first try

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,16 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        //number is 478
+        int r1 = number / 100; // 4 ++
+        int n2 = r1 * 100; // 400
+        int n3 = number - n2; // 78
+        int n4 = n3 / 10; // 7
+        int r2 = n4 * 10; // 70 ++
+        int n5 = n3 - r2; // 8
+        int r3 = n5 * 100; // 800 ++
+
+        System.out.println(r1 + r2 + r3);
+
     }
 }
